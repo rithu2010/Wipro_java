a4c9c0e90be9b16c7587d1f572944f263c1b8fe8
Rithika <rithika200804@gmail.com>
2025-07-18 20:06:13 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,12 @@
+package Wipro;
+import java.util.*;
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Welcome "+args[0];
+		System.out.print(str);
+
+	}
+
+}
