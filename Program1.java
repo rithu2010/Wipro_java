32ed5e7ac3d1fab78163a5c0d4c91316075a3eb4
Rithika <rithika200804@gmail.com>
2025-07-18 20:04:40 +0530

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,12 @@
+package Wipro;
+import java.util.*;
+public class Program1 {
+	 static public void main(String[] args) {
+		
+		String str=args[0]+"Technologies";
+		String str1=args[1];
+		System.out.println(str);
+		System.out.println(str1);
+	}
+}
+
